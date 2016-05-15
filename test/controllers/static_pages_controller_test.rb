b52ac7f11328e841a366892e0c1b4b1421850c1c
Andi <andicbuchanan@gmail.com>
2016-05-15 01:17:28 +0000

    added helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Cheese Talk"
+    assert_select "title", "Cheese Talk"
   end
 
   test "should get help" do
